0bbf93e64094cb9720cd629b3a66fc5674a47624
Samgrass <samuelegrassetti01@gmail.com>
2024-06-13 16:49:59 +0200

    aggiungo elementi e commenti js

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+// creo una griglia 10x10 qunado l'utente clicca un bottone, ogni casella che viene cliccata cambia colore e mostra il suo numero
+// collego elemento bottone e elemento container
+
+// aggiungo evento click 
+
+    // ciclo da 100 giri
+
+        // creo un elemento "quadrato"
+
+        // aggiungo click all'elemento per dare classe e far apparire il numero index
+
