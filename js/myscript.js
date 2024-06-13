b96898c46612e805175221163057362954dcc69d
Samgrass <samuelegrassetti01@gmail.com>
2024-06-13 18:31:11 +0200

    aggiunto click per dare classe a square

--- js/myscript.js
+++ js/myscript.js
@@ -2,23 +2,35 @@
 // collego elemento bottone e elemento container
 const btnPlay = document.getElementById("play");
 const squareContainer = document.querySelector(".row");
-
 // aggiungo evento click 
 btnPlay.addEventListener ('click',
     function () {
-        
+
         // ciclo da 100 giri
-        for (let i = 0; i < 100; i++) {
+        for (let i = 1; i <= 100; i++) {
             // creo un elemento "quadrato"
-            square = document.createElement("div");
+            let square = document.createElement("div");
             square.classList.add("square");
+            
+            // aggiungo click all'elemento per dare classe e far apparire il numero index
+            square.addEventListener('click', 
+                function () {
+                    square.classList.toggle("clicked");
+
+                    console.log(square);
+                }
+            )
+            console.log(square);
             squareContainer.append(square);
-        }
+        } 
     }
 )
-    
 
-        
 
-        // aggiungo click all'elemento per dare classe e far apparire il numero index
+
+
+
+
+
+
 
