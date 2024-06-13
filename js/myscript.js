f90e833941462e02e8c373bb64eb730f26c1c2d1
Samgrass <samuelegrassetti01@gmail.com>
2024-06-13 17:34:42 +0200

    aggiungo click per creare 100 quadrati

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,24 @@
 // creo una griglia 10x10 qunado l'utente clicca un bottone, ogni casella che viene cliccata cambia colore e mostra il suo numero
 // collego elemento bottone e elemento container
+const btnPlay = document.getElementById("play");
+const squareContainer = document.querySelector(".row");
 
 // aggiungo evento click 
+btnPlay.addEventListener ('click',
+    function () {
+        
+        // ciclo da 100 giri
+        for (let i = 0; i < 100; i++) {
+            // creo un elemento "quadrato"
+            square = document.createElement("div");
+            square.classList.add("square");
+            squareContainer.append(square);
+        }
+    }
+)
+    
 
-    // ciclo da 100 giri
-
-        // creo un elemento "quadrato"
+        
 
         // aggiungo click all'elemento per dare classe e far apparire il numero index
 
